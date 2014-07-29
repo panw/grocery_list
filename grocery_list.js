2428b5796848cf04471b82ad51a7045648ed6e23
Pan W <panthongw@gmail.com>
2014-07-28 22:45:11 -0700

    data displayed in the template is now dynamic and added some database seeding code

--- grocery_list.js
+++ grocery_list.js
@@ -1,9 +1,18 @@
-if (Meteor.isClient) {
+Items = new Meteor.Collection('items');
 
+if(Meteor.isClient){
+  Template.groceryList.helpers({
+    items: function() {
+      return Items.find();
+    }
+  });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if(Meteor.isServer){
+  if (Items.find().count() === 0) {
+    Items.insert({ name: "Baguette" });
+    Items.insert({ name: "Butter" });
+    Items.insert({ name: "Jam" });
+    Items.insert({ name: "Coconut" });
+  }
 }
