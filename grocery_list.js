a798bf618f7020df1ac9c00b4c6862912a719589
Pan W <panthongw@gmail.com>
2014-07-31 00:07:34 -0700

    add an event handler to detect when items get clicked and hide the item list

--- grocery_list.js
+++ grocery_list.js
@@ -1,6 +1,22 @@
 Items = new Meteor.Collection('items');
 
 if(Meteor.isClient){
+
+  Template.groceryList.events({
+    'click li' : function(event, template){
+      var listItem = event.currentTarget; // get the list item being clicked on
+      $('#'+listItem.id).hide(); // hide li with matching id
+
+      // update check attribute for the item in the database
+      Items.update(listItem.id, { $set : {checked:true} }, function(error){
+        // if error occurs print it out
+        if(error){
+          console.log(error.reason);
+        }
+      });
+    }
+  });
+
   Template.groceryList.helpers({
     items: function() {
       return Items.find();
@@ -10,9 +26,9 @@ if(Meteor.isClient){
 
 if(Meteor.isServer){
   if (Items.find().count() === 0) {
-    Items.insert({ name: "Baguette"});
-    Items.insert({ name: "Butter"});
-    Items.insert({ name: "Jam"});
-    Items.insert({ name: "Coconut"});
+    Items.insert({ name: "Baguette", checked: false });
+    Items.insert({ name: "Butter", checked: false });
+    Items.insert({ name: "Jam", checked: false });
+    Items.insert({ name: "Coconut", checked: false });
   }
 }
